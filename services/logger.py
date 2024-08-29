1ceb126a4c47562b89527543f6b4786ba06a1d46
NinaSayers <massypaz2001git@gmail.com>
2024-08-29 01:31:21 -0400

    add .venv

--- services/logger.py
+++ services/logger.py
@@ -12,13 +12,4 @@ class Logger:
     
     def warn(self, log): 
         current_time = datetime.now().strftime('%H:%M:%S')
-        print(f'[WARN] {current_time}: {log}')
-
-
-
-     
-
-
-        
-
-            
+        print(f'[WARN] {current_time}: {log}')       
