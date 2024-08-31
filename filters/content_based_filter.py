7680b714eada225c3bb347a862c0d209456cc1e4
Albaro Suarez Valdes <albarosuarez02@gmail.com>
2024-08-31 10:47:42 -0400

    refac: enhance content base filter

--- filters/content_based_filter.py
+++ filters/content_based_filter.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from typing import List
+from typing import List, Optional
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
@@ -15,66 +15,55 @@ class ContentBaseFilter(FilterBase):
         super().__init__()
         self.tfidf_vectorizer = TfidfVectorizer(stop_words='english')
 
-    def apply_filter(self, context: Context) -> FilterResultModel:
-        session = SessionLocal()
-        interactions_repository = InteractionRepository(session)
-        self.logger.info("Applying content based filters")
-        try:
-            product_descriptions = [product.getProductDescribed() for product in context.products]
+    def apply_filter(self, context: Context) -> Optional[FilterResultModel]:
+        """Apply content-based filtering to generate product recommendations."""
+        if not context.products:
+            self.logger.warn("No products provided in context.")
+            return None
 
-            tfidf_matrix = self.tfidf_vectorizer.fit_transform(product_descriptions)
+        if not context.userId:
+            self.logger.warn("No user ID provided in context.")
+            return None
 
-            user_vector = self.get_user_vector(context.userId, context.products, tfidf_matrix)
-            if user_vector is None:
-                self.logger.info("No valid user vector found.")
-                return []
+        self.logger.info(f"Applying content-based filters to {len(context.products)} products, expecting {context.limit} filtered.")
 
-            cosine_similarities = cosine_similarity([user_vector], tfidf_matrix).flatten()
+        with SessionLocal() as session:
+            interactions_repository = InteractionRepository(session)
+            try:
+                product_descriptions = self.get_product_descriptions(context.products)
+                tfidf_matrix = self.tfidf_vectorizer.fit_transform(product_descriptions)
 
-            interacted_product_ids = {interaction.product_id for interaction in interactions_repository.get_interactions_by_user(context.userId)}
-            recommendations = []
-            for i in cosine_similarities.argsort()[::-1]:
-                if context.products[i].unique_id not in interacted_product_ids:
-                    recommendation = RecommendationModel(
-                        product_id=context.products[i].unique_id,
-                        similarity_score=cosine_similarities[i]
-                    )
-                    recommendations.append(recommendation)
-                if len(recommendations) >= context.limit: 
-                    break
+                user_vector = self.get_user_vector(context.userId, context.products, tfidf_matrix, interactions_repository)
+                if user_vector is None:
+                    self.logger.warn("No valid user vector found.")
+                    return None
 
-            # Create the FilterResultModel with the list of recommendations
-            filter_result = FilterResultModel(
-                user_id=context.userId,
-                recommendations=recommendations
-            )
+                recommendations = self.get_recommendations(context, tfidf_matrix, user_vector, interactions_repository)
 
-            return filter_result
+                return FilterResultModel(user_id=context.userId, recommendations=recommendations)
 
-        except Exception as e:
-            self.logger.error(f"An error occurred while applying the Content Based filter: {str(e)}")
-            return []
-        finally:
-            session.close()
+            except Exception as e:
+                self.logger.error(f"An error occurred while applying the Content-Based filter: {str(e)}")
+                return None
 
-    def get_user_vector(self, user_id, products, tfidf_matrix):
-        session = SessionLocal()
-        interactions_repository = InteractionRepository(session)
+    def get_product_descriptions(self, products: List) -> List[str]:
+        """Extract descriptions from a list of products."""
+        return [product.getProductDescribed() for product in products]
+
+    def get_user_vector(self, user_id: int, products: List, tfidf_matrix: np.ndarray, interactions_repository: InteractionRepository) -> Optional[np.ndarray]:
+        """Calculate the user's vector based on their interactions with products."""
         try:
             interaction_weights = []
             product_vectors = []
+
             user_interactions = interactions_repository.get_interactions_by_user(user_id)
             for interaction in user_interactions:
                 product = next((p for p in products if p.unique_id == interaction.product_id), None)
                 if product:
                     index = products.index(product)
                     product_vectors.append(tfidf_matrix[index].toarray().flatten())
-                    switcher = {
-                        "view": 2,
-                        "like": 3,
-                        "purchase": 5
-                    }
-                    weight = switcher.get(interaction.interaction_type, 1)
+
+                    weight = self.get_interaction_weight(interaction.interaction_type)
                     interaction_weights.append(weight)
 
             if not product_vectors:
@@ -88,5 +77,33 @@ class ContentBaseFilter(FilterBase):
             self.logger.error(f"An error occurred when trying to get the user vector: {str(e)}")
             return None
 
-        finally:
-            session.close()
+    def get_interaction_weight(self, interaction_type: str) -> int:
+        """Map interaction types to their corresponding weights."""
+        return {
+            "view": 2,
+            "like": 3,
+            "purchase": 5
+        }.get(interaction_type, 1)
+
+    def get_recommendations(self, context: Context, tfidf_matrix: np.ndarray, user_vector: np.ndarray, interactions_repository: InteractionRepository) -> List[RecommendationModel]:
+        """Generate a list of recommendations based on cosine similarity."""
+        cosine_similarities = cosine_similarity([user_vector], tfidf_matrix).flatten()
+
+        interacted_product_ids = {
+            interaction.product_id
+            for interaction in interactions_repository.get_interactions_by_user(context.userId)
+        }
+
+        recommendations = []
+        for i in cosine_similarities.argsort()[::-1]:
+            if context.products[i].unique_id not in interacted_product_ids:
+                recommendations.append(
+                    RecommendationModel(
+                        product_id=context.products[i].unique_id,
+                        similarity_score=cosine_similarities[i]
+                    )
+                )
+            if len(recommendations) >= context.limit:
+                break
+
+        return recommendations
