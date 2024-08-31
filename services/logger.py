344e0c5cb0853deffb393ed35509718f721d4708
Albaro Suarez Valdes <albarosuarez02@gmail.com>
2024-08-31 10:40:41 -0400

    feat: add colors to logger

--- services/logger.py
+++ services/logger.py
@@ -1,15 +1,20 @@
 from datetime import datetime
+from colorama import Fore, Style, init
+
+# Inicializar colorama (importante para Windows)
+init(autoreset=True)
 
 class Logger:
-    '''Basic logger, with hope to be changed some day'''
+    '''Basic logger with color output'''
+    
     def info(self, log): 
         current_time = datetime.now().strftime('%H:%M:%S')
-        print(f'[INFO] {current_time}: {log}')
+        print(f'{Fore.GREEN}[INFO] {current_time}: {log}{Style.RESET_ALL}')
     
     def error(self, log): 
         current_time = datetime.now().strftime('%H:%M:%S')
-        print(f'[ERROR] {current_time}: {log}')
+        print(f'{Fore.RED}[ERROR] {current_time}: {log}{Style.RESET_ALL}')
     
     def warn(self, log): 
         current_time = datetime.now().strftime('%H:%M:%S')
-        print(f'[WARN] {current_time}: {log}')       
+        print(f'{Fore.YELLOW}[WARN] {current_time}: {log}{Style.RESET_ALL}')
