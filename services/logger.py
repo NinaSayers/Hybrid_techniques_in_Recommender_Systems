5b5d96c8d8b781827177de5ae739d6ab0ae2468b
Albaro Suarez Valdes <albarosuarez02@gmail.com>
2024-09-01 14:33:57 -0400

    feat: extend logger to print markdown tables

--- services/logger.py
+++ services/logger.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 from colorama import Fore, Style, init
+from IPython.display import display, Markdown
+import io
 
-# Inicializar colorama (importante para Windows)
 init(autoreset=True)
 
 class Logger:
     '''Basic logger with color output'''
-    
     def info(self, log): 
         current_time = datetime.now().strftime('%H:%M:%S')
         print(f'{Fore.GREEN}[INFO] {current_time}: {log}{Style.RESET_ALL}')
@@ -18,3 +18,18 @@ class Logger:
     def warn(self, log): 
         current_time = datetime.now().strftime('%H:%M:%S')
         print(f'{Fore.YELLOW}[WARN] {current_time}: {log}{Style.RESET_ALL}')
+    
+    def print_markdown_table(self, headers, rows):
+        # Create table in Markdown format
+        markdown_table = "| " + " | ".join(headers) + " |\n"
+        markdown_table += "|---" * len(headers) + "|\n"
+        for row in rows:
+            markdown_table += "| " + " | ".join(str(cell) for cell in row) + " |\n"
+        
+        # Print the Markdown table
+        # Use a file-like object to capture Markdown output in a Jupyter notebook
+        buf = io.StringIO()
+        buf.write(markdown_table)
+        buf.seek(0)
+        markdown_output = buf.getvalue()
+        display(Markdown(markdown_output))
